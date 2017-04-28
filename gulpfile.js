d577ed25270f67002342c2d9f9641b73780ae323
Alexandre Masy <hello@alexandremasy.com>
2017-04-28 10:13:23 +0200

    Start implementing the testing framework

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,32 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+
+
+///////////////////////////////////////////////////////////////////
+
+/**
+ *  Task: Styles [dev]
+ *  Build the sass files for development
+ *
+ *  @version 1.1
+ *  @author Afelio - Alexandre Masy <a.masy@afelio.be>
+ **/
+gulp.task('styles.dev', function(){
+  var sass = require('gulp-sass');
+  var autoprefixer = require('gulp-autoprefixer');
+
+  gulp.src(['src/styles/app.scss', 'src/styles/styleguide.scss'])
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('build/statics/styles/'))
+    .pipe(connect.reload())
+    ;
+});
+
+
+//
+//  Base on `scripts-to-rule-them-all`
+//
+//  scripts/bootstrap: is used solely for fulfilling dependencies of the project
+//  scripts 
+//
